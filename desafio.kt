7249bdee3c66d7c4800b3cdf9d974bd3c4fb6f55
AlexeiRuizR <118298838+AlexeiRuizR@users.noreply.github.com>
2022-12-19 13:38:11 -0300

    Arreglos en variables y clase Usuario

--- desafio.kt
+++ desafio.kt
@@ -2,11 +2,11 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, AVANÇADO }
 
-class Usuario
+class Usuario (val name: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
 
     val inscritos = mutableListOf<Usuario>()
     
