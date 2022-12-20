64fcee47256af98bc5c65fae54aae77ef7bb0650
AlexeiRuizR <118298838+AlexeiRuizR@users.noreply.github.com>
2022-12-19 23:27:16 -0300

    Avançando en el codigo

--- desafio.kt
+++ desafio.kt
@@ -1,12 +1,12 @@
 // [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
 
-enum class Nivel { BASICO, INTERMEDIARIO, AVANÇADO }
+enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
 class Usuario (val name: String)
 
 data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
-data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) {
+data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>, val nivel: String) {
 
     val inscritos = mutableListOf<Usuario>()
     
@@ -16,6 +16,13 @@ data class Formacao(val nome: String, val conteudos: List<ConteudoEducacional>) 
 }
 
 fun main() {
+val nivel = Nivel.BASICO
+val message = when (nivel) {                      // 3
+        Nivel.BASICO -> "It's nivel basico"
+        Nivel.INTERMEDIARIO -> "It's nivel intermediario"
+        Nivel.AVANCADO -> "It's nivel avançado"
+        }
+        println(message)
     TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
     TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
 }
